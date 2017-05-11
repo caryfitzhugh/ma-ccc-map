0e1474651c769bc7241066a4ca31e20ce1954296
Steve Signell <steve@frontierspatial.com>
2017-05-11 15:29:34 -0400

    fixed vt mask white renderer

--- src/js/renderers/vt_mask.js
+++ src/js/renderers/vt_mask.js
@@ -1,14 +1,14 @@
 /*global L, Renderers, GEOSERVER */
 RendererTemplates.wms("vt_mask", {
   update_legend: function (active_layer) {
-    return CDN(GEOSERVER + "/wms?request=GetLegendGraphic&LAYER=vt:vt_mask&style="+active_layer.parameters.style + "&format=image/png");
+    return CDN(GEOSERVER + "/wms?request=GetLegendGraphic&LAYER=vt:vt_mask&styles="+active_layer.parameters.style + "&format=image/png");
   },
   url: CDN (GEOSERVER + "/vt/wms/"),
   wms_opts: function (active_layer) {
         return {
           layers: 'vt:vt_mask',
           format: 'image/png',
-          style: active_layer.parameters.style,
+          styles: active_layer.parameters.style,
           transparent: true,
           opacity: 1,
           zIndex: 100
