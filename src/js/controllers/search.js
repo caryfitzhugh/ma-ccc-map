9ce29e66894df6ae4bcc31e2e7c333fb01eb05dd
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-06-09 16:49:53 -0400

    adds MA sheild and changes

--- src/js/controllers/search.js
+++ src/js/controllers/search.js
@@ -2,7 +2,7 @@ Controllers.Search = {
   execute_search: function (search_str, callback) {
     $.getJSON(
       /*Not on CDN, b/c the port is not 8080 */
-      "http://api.geonames.org/searchJSON?username=frontierspatial&featureClass=P&maxRows=5&country=US&adminCode1=VT&name_startsWith=" + encodeURIComponent(search_str) + "&callback=?",
+      "http://api.geonames.org/searchJSON?username=frontierspatial&featureClass=P&maxRows=5&country=US&adminCode1=MA&name_startsWith=" + encodeURIComponent(search_str) + "&callback=?",
       function (data, textStatus, jqXHR) {
         callback(search_str, data.geonames);
       }
