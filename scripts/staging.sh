1d5f4ff73892176171b7060fb9bdfcfecace7a28
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-07-05 10:35:06 -0400

    testing

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -6,6 +6,7 @@ echo "<h2>Deployed " `date` "</h2>" >> index.html
 
 echo "<h3>" `git log -1` "</h3>" >> index.html
 git add -f dist/* index.html
+vim dist/map_viewer.html
 git commit -m "Releasing"
 echo "Pushing to gh-pages"
 git push -f origin `git rev-parse --symbolic-full-name --abbrev-ref HEAD`:gh-pages
