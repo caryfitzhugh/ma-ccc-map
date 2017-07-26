37f8b07b218667a443cbcfb269b0260c50430b41
Steve Signell <steve@frontierspatial.com>
2017-07-26 10:25:56 -0400

    added ej layer

---
+++ src/js/renderers/tax_parcels.js
@@ -0,0 +1,58 @@
+RendererTemplates.wms("tax_parcels", {
+  parameters: {
+    opacity: 70,
+    options: {
+
+    }
+  },
+  clone_layer_name: function(active_layer) {
+    return active_layer.name;
+  },
+  url: CDN("http://giswebservices.massgis.state.ma.us/geoserver/wms"),
+  wms_opts:(active_layer) => {
+    //var year = active_layer.parameters.year;
+    return  {
+      layers: 'massgis:GISDATA.L3_TAXPAR_POLY_ASSESS',
+      format: "image/png",
+      opacity: 0,
+      zIndex: -1,
+      transparent: true,
+    };
+  },
+  get_feature_info_url: function (active_layer) {
+    //var year = active_layer.parameters.year;
+    return CDN("http://giswebservices.massgis.state.ma.us/geoserver/wms") +
+          "?SERVICE=WMS&VERSION=1.1.1&"+
+          "REQUEST=GetFeatureInfo&LAYERS=massgis:GISDATA.L3_TAXPAR_POLY_ASSESS&"+
+          "QUERY_LAYERS=massgis:GISDATA.L3_TAXPAR_POLY_ASSESS&"+
+          "STYLES=&"+
+          "BBOX=<%= bbox %>&"+
+          "FEATURE_COUNT=5&"+
+          "HEIGHT=<%= height %>&"+
+          "WIDTH=<%= width %>&"+
+          "FORMAT=text%2Fhtml&"+
+          "INFO_FORMAT=application%2Fjson&"+
+          "SRS=EPSG:4326&"+
+          "X=<%= x %>&Y=<%= y %>";
+  },
+  legend_template: `
+      <div class='detail-block show-confidence'>
+        <label> Legend: </label>
+        <img src='{{CDN("http://giswebservices.massgis.state.ma.us/geoserver/wms?request=GetLegendGraphic&LAYER=massgis:GISDATA.L3_TAXPAR_POLY_ASSESS&format=image/png")}}'/> Environmental Justice Population
+      </div>
+  `,
+  info_template: `
+      <div class='col-xs-2'>
+        <label> {{name}} </label>
+      </div>
+      <div class='col-xs-10'>
+        {{#json.features}}
+          <div>
+            Owner: {{properties.OWNER1}}
+          </div>
+        {{ else }}
+          Unknown / No Response
+        {{/json.features}}
+      </div>
+  `
+});
