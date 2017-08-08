627ba62e28a3f4e4d83b0939e324bc4fbded34e0
Steve Signell <steve@frontierspatial.com>
2017-08-08 06:32:46 -0400

    added electric, gas providers

---
+++ src/js/renderers/electric_providers.js
@@ -0,0 +1,60 @@
+RendererTemplates.wms("electric_providers", {
+  parameters: {
+    opacity: 70,
+/*    min_zoom: 14,
+    max_zoom: 20,*/
+    options: {
+
+    }
+  },
+  clone_layer_name: function(active_layer) {
+    return active_layer.name;
+  },
+  url: CDN("http://giswebservices.massgis.state.ma.us/geoserver/wms"),
+  wms_opts:(active_layer) => {
+    //var year = active_layer.parameters.year;
+    return  {
+      layers: 'massgis:GISDATA.TOWNS_POLY_V_ELEC',
+      format: "image/png",
+      opacity: 0,
+      zIndex: -1,
+      transparent: true,
+    };
+  },
+  get_feature_info_url: function (active_layer) {
+    //var year = active_layer.parameters.year;
+    return CDN("http://giswebservices.massgis.state.ma.us/geoserver/wms") +
+          "?SERVICE=WMS&VERSION=1.1.1&"+
+          "REQUEST=GetFeatureInfo&LAYERS=massgis:GISDATA.TOWNS_POLY_V_ELEC&"+
+          "QUERY_LAYERS=massgis:GISDATA.TOWNS_POLY_V_ELEC&"+
+          "STYLES=&"+
+          "BBOX=<%= bbox %>&"+
+          "FEATURE_COUNT=5&"+
+          "HEIGHT=<%= height %>&"+
+          "WIDTH=<%= width %>&"+
+          "FORMAT=text%2Fhtml&"+
+          "INFO_FORMAT=application%2Fjson&"+
+          "SRS=EPSG:4326&"+
+          "X=<%= x %>&Y=<%= y %>";
+  },
+  legend_template: `
+      <div class='detail-block show-confidence'>
+        <label> Legend: </label>
+        <img src='{{CDN("http://giswebservices.massgis.state.ma.us/geoserver/wms?request=GetLegendGraphic&LAYER=massgis:GISDATA.TOWNS_POLY_V_ELEC&format=image/png")}}'/>
+      </div>
+  `,
+  info_template: `
+      <div class='col-xs-2'>
+        <label> {{name}} </label>
+      </div>
+      <div class='col-xs-10'>
+        {{#json.features}}
+          <div>
+            {{properties.ELEC_LABEL}}
+          </div>
+        {{ else }}
+          Unknown / No Response
+        {{/json.features}}
+      </div>
+  `
+});
