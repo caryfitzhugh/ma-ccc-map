7cedf1eda8a816b48ba61dec731aaa8fab66f546
Cary FitzHugh <cary.fitzhugh@gmail.com>
2017-10-20 22:26:15 -0400

    Tide gauge done.

--- src/js/renderers/tide_guage_slr.js
+++ src/js/renderers/tide_guage_slr.js
@@ -4,7 +4,6 @@ RendererTemplates.geojson_points("tide_guage_slr", {
     rcps: [4.5,
            8.5],
     percentiles: [
-        "All",
         17,
         50,
         83,
@@ -15,7 +14,12 @@ RendererTemplates.geojson_points("tide_guage_slr", {
         2020,
         2030,
         2040,
-        2050
+        2050,
+        2060,
+        2070,
+        2080,
+        2090,
+        2100
         ],
     options: {
       year_indx: 0,
@@ -29,36 +33,42 @@ RendererTemplates.geojson_points("tide_guage_slr", {
   selectData: function (active_layer, all_data) {
     let year = active_layer.parameters.years[active_layer.parameters.options.year_indx];
     let rcp = active_layer.parameters.rcps[active_layer.parameters.options.rcp_indx];
+    let percentile = active_layer.parameters.percentiles[active_layer.parameters.options.percentile_indx];
+    let all_vals = [];
+    let max = 0;
+    all_data.forEach((feature) => {
+      all_vals = all_vals.concat([feature.properties.p17, feature.properties.p50,
+                                  feature.properties.p83, feature.properties.p99]);
+    });
+    max = _.max(all_vals);
 
-    return _.reduce(all_data, function (all, feature) {
+    let data = _.reduce(all_data, function (all, feature) {
       if (year === feature.properties.year) {
         if (rcp === feature.properties.rcp) {
-          all.push(feature);
+          let val = feature.properties["p"+percentile];
+
+          let new_feature = _.cloneDeep(feature);
+          new_feature.properties.value = [percentile, val, ( val / max)];
+          all.push(new_feature);
         }
       }
 
       return all;
     }, []);
+
+    return data;
   },
   pointToLayer: function (active_layer, feature, latlng) {
-    // Find
-    let percentile = active_layer.parameters.percentiles[active_layer.parameters.options.percentile_indx];
-    let values = [];
-    let params = active_layer.parameters;
-    if (percentile === 'All') {
-      values.push(['17', feature.properties.p17])
-      values.push(['50', feature.properties.p50])
-      values.push(['83', feature.properties.p83])
-      values.push(['99', feature.properties.p99])
-    } else {
-      values.push([percentile, feature.properties["p"+percentile]]);
-    }
+    let v = feature.properties.value;
+    let height = v[2] * 100;
     return L.marker(latlng, {
+
         icon: L.divIcon({
             className: 'tide-guage-slr-icon',
-            html: values.map((v) => {
-                return `<div class='bar percentile percentile-${v[0]}'> ${v[1]} </div>`;
-              }).join(" ")
+            html: `<div class='bar percentile percentile-${v[0]}'>
+                <div class='bar' style='height: ${height}%; width: 100%;'> </div>
+                <div class='label'>${v[1]}</div>
+            </div>`
             })
     });
   },
@@ -66,9 +76,18 @@ RendererTemplates.geojson_points("tide_guage_slr", {
   popupContents: function (feature) {
     var index = feature.properties.reason_cls
 
-    return "<strong>Name: " + feature.properties.name + "</strong></br>" +
-           "Year: " + feature.properties.year + "<br/>"+
-          "</br>"+
+    return `<strong>Name: ${feature.properties.name}</strong></br>
+           <strong>Year:</strong>${feature.properties.year}<br/>
+           <strong>RCP:</strong>${feature.properties.rcp}<br/>
+           <table>
+              <thead> <tr> <th> Percentile</th><th> Value </th></tr></thead>
+              <tbody>
+                <tr><td>17%</td><td>${feature.properties.p17}</td> </tr>
+                <tr><td>50%</td><td>${feature.properties.p50}</td> </tr>
+                <tr><td>83%</td><td>${feature.properties.p83}</td> </tr>
+                <tr><td>99%</td><td>${feature.properties.p99}</td> </tr>
+              </tbody>
+           </table>` +
            Renderers.utils.zoom_to_location_link( feature.geometry );
   },
   legend_template: `
