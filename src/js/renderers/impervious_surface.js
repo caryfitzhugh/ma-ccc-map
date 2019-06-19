f2c1acba81bf358c8c143945b67547e0e7fb9745
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-06-18 22:33:38 -0400

    gives impervious ground layer understandable legend

--- src/js/renderers/impervious_surface.js
+++ src/js/renderers/impervious_surface.js
@@ -3,12 +3,16 @@ RendererTemplates.wms("impervious_surface", {
     opacity: 70,
     options: {
 
-    }
+    },
+    color_buckets: [
+
+    ]
   },
   clone_layer_name: function(active_layer) {
     return active_layer.name;
   },
   url: CDN("https://www.mrlc.gov/geoserver/wms?"),
+
   wms_opts:(active_layer) => {
     //var year = active_layer.parameters.year;
     return  {
@@ -19,6 +23,33 @@ RendererTemplates.wms("impervious_surface", {
       transparent: true,
     };
   },
+
+  on_load: (active_layer) => {
+    let durl = CDN("https://www.mrlc.gov/geoserver/wms?request=GetLegendGraphic&LAYER=mrlc_display:NLCD_2016_Impervious_L48&format=application/json");
+    if (active_layer.parameters.loaded_legend) {
+      return;
+    }
+
+    active_layer.parameters.loaded_legend = true;
+
+    $.ajax({
+      cache: true,
+      dataType: "json",
+      url: durl,
+      success: function (json) {
+        let buckets = json.Legend[0].rules[0].symbolizers[0].Raster.colormap.entries;
+        active_layer.parameters.color_buckets = [];
+        for (let i=0; i < buckets.length; i += Math.floor((buckets.length - 1) / 8)) {
+          active_layer.parameters.color_buckets.push(buckets[i]);
+        }
+        Views.ControlPanel.fire("tile-layer-loaded", active_layer, true);
+      },
+      error:   function (err) {
+        Views.ControlPanel.fire("tile-layer-loading-error", active_layer);
+      }
+    });
+  },
+
   get_feature_info_url: function (active_layer) {
     //var year = active_layer.parameters.year;
     return CDN("https://www.mrlc.gov/geoserver/wms") +
@@ -36,10 +67,17 @@ RendererTemplates.wms("impervious_surface", {
           "X=<%= x %>&Y=<%= y %>";
   },
   legend_template: `
-      <div class='detail-block show-confidence'>
-        <label> Legend: </label>
-        <img src='{{CDN("https://www.mrlc.gov/geoserver/wms?request=GetLegendGraphic&LAYER=mrlc_display:NLCD_2016_Impervious_L48&format=image/png")}}'/>
-      </div>
+    <div class='detail-block legend'>
+      <div class='color-legend '>% Impervious</div>
+      {{#parameters.color_buckets}}
+        <div class='color-block' style="width: {{100.0 / parameters.color_buckets.length}}%;">
+              <svg width="100" height="100">
+                <rect height="100" width="100" style="fill: {{color}}; stroke: {{color}}; opacity:{{opacity}}; "/>
+              </svg>
+              <label>{{label}}</label>
+        </div>
+      {{/parameters.color_buckets}}
+    </div>
   `,
   info_template: `
       <div class='col-xs-2'>
