63ca9801437ff093ac86e43840064bec74676dc0
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-08-19 22:28:41 -0400

    write out temp

--- src/js/renderers/ma_observed_avg_temp.js
+++ src/js/renderers/ma_observed_avg_temp.js
@@ -1,6 +1,6 @@
 RendererTemplates.ma_observed_climate_data('ma_observed_avg_temp', {
   title: "Average Temperature",
-  legend: "Observed Average Temp. (&deg;F)",
+  legend: "Observed Average Temperature (&deg;F)",
   legend_units: " &deg; F",
   legend_precision: 1,
   data_url: "https://repository.nescaum-ccsc-dataservices.com/data/ma_observed/avgtemp.json",
--- src/js/renderers/ma_projected_avg_temp.js
+++ src/js/renderers/ma_projected_avg_temp.js
@@ -1,6 +1,6 @@
 RendererTemplates.ma_projected_climate_data('ma_projected_avg_temp', {
   title: "Average Temperature",
-  legend: "Projected change in Average Temp. (&deg;F)",
+  legend: "Projected change in Average Temperature (&deg;F)",
   legend_units: " &deg;F",
   legend_precision: 2,
   info_precision: 2,
